eaee2b0eac6d8895639e13981f67203ff79dc927
Beau Sorin <beausorinwshs@gmail.com>
2024-04-22 15:13:58 -0500

    Add username page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
   def index
   end
+
+  def beausorin
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root 'pages#index'
+
+  get 'beausorin', to: 'pages#beausorin', as: 'beausorin'
 end
